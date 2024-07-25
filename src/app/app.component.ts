01b03dd35987eeb0b141b5ae39b3512c752a3474
Joao Pedro <dp.joaopedro@gmail.com>
2024-07-24 23:34:49 -0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recuperacao';
+export class AppComponent implements OnChanges {
+  @Input('route') route!: string;
+
+  constructor(private router: Router) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('navegando para', changes['route'].currentValue)
+    this.router.navigate([this.route], { skipLocationChange: true })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent,
+    BarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'recuperacao/foo', component: FooComponent },
+      { path: 'recuperacao/bar', component: BarComponent },
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    const ce = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('mf-recuperacao', ce);
+  }
+
+}
